907c20d8233e57977f6006e554b5ae8505625092
Julia Anisio <julia.an.nogueira@gmail.com>
2025-07-30 20:43:01 -0300

    New feature: new tests for ft_memcpy

--- test/test_ft_memcpy.c
+++ test/test_ft_memcpy.c
@@ -48,6 +48,44 @@ int main (void){
 
     ASSERT_MEM_EQ(dst1, dst2, 9, "ft_memcpy should copy 9 bytes from src to dst");
 
+    //TEST 2 - copia os dados corretos
+    
+    char src2[] = {'A', '\0', 'B', '\0', 'C', '\0', 'D', '\0', 'E'};
+    char dst[9];
+    char expected[9];
+
+    memcpy(expected, src2, 9);
+    ft_memcpy(dst,  src2, 9);
+    ASSERT_MEM_EQ(dst, expected, 9, "should copy 9 bytes from src");
+
+    //TEST3 - copia 0 bytes, destino não deve mudar
+    char src3[] = "Hello";
+    char dst_1[] = "XXXXX";
+    char dst_2[] = "XXXXX";
+
+    memcpy(dst_1, src3, 0);
+    ft_memcpy(dst_2, src3, 0);
+    ASSERT_MEM_EQ(dst_1, dst_2, 5, "copy 0 bytes: should do nothing");
+
+    //TEST4 - src e dst são o mesmo ponteiro
+
+    char buffer[] = "ABCDE";
+    char *data = buffer;
+    char *dt = buffer;
+    memcpy(data, dt, 5);
+    ft_memcpy(data, dt, 5);
+    ASSERT_MEM_EQ(data, dt, 5, "src == dst: should still succeed");
+
+    //TEST 5 - ponteiros nulos com n == 0
+
+    char *ptr1 = NULL;
+    char *ptr2 = NULL;
+
+    void *ret1 = memcpy(ptr1, ptr2, 0);
+    void *ret2 = ft_memcpy(ptr1, ptr2, 0);
+
+    ASSERT_TRUE(ret1 == ret2, "ft_memcpy(NULL, NULL, 0) returns same as memcpy");
+
+
     return (fail);
 }
-
