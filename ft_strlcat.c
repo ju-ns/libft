2a7256d4ae39d4d1ba5139092e7e60bbb0db1dc3
Julia Anisio <julia.an.nogueira@gmail.com>
2025-08-02 17:46:39 -0300

    New feature: ft_strlcat new tests

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -15,9 +15,21 @@
 size_t ft_strlcat(char *dst, const char *src, size_t size)
 {
     size_t i;
+    size_t src_len;
+    size_t dst_len;
 
     i = 0;
-    while(src[i])
-        src[i++];
+    src_len = ft_strlen(src);
+    dst_len = ft_strlen(dst);
     
+    if(size <= dst_len)
+        return(src_len + size);
+    while(src[i] && (dst_len + i < size - 1))
+    {
+        dst[dst_len + i] = src[i];
+        i++;
+    }
+    dst[dst_len + i] = '\0';
+    
+    return (dst_len + src_len);     
 }
--- test/test_ft_strlcat.c
+++ test/test_ft_strlcat.c
@@ -38,10 +38,53 @@ int main (){
     const char *src = "World";
 
     size_t ret_std = strlcat(dst1, src, sizeof(dst1));
-    size_t ret_ft = ft_strlcat(dst1, src, sizeof(dst2));
+    size_t ret_ft = ft_strlcat(dst2, src, sizeof(dst2));
 
     ASSERT_STR_EQ(dst1, dst2, "ft_strlcat should produce the same final string");
     TEST("ft_strlcat should return the correct total length", ret_std == ret_ft);
+
+    //TEST 2: Concatenar com espaço insuficiente
+    char dst3[10] = "ABC";
+    char dst4[10] = "ABC";
+    const char *src1 = "DEFGHIJKL";
+
+    size_t ret_std_b = strlcat(dst3, src1, 6);
+    size_t ret_ft_b = ft_strlcat(dst4, src1, 6);
+
+    ASSERT_STR_EQ(dst3, dst4, "strlcat should truncate when buffer is too small");
+    TEST("strlcat should return length of src + min (size, dst_len)", ret_std_b == ret_ft_b);
+
+    //TEST 3:  size = 0
+    char dst5[10] = "ignore";
+    char dst6[10] = "ignore";
+    const char *src2 = "HELLO";
+
+    size_t ret_std_c = strlcat(dst5, src2, 0);
+    size_t ret_ft_c = ft_strlcat(dst6, src2, 0);
+
+    TEST("strlcat with size 0 must return strlen(src)", ret_std_c == ret_ft_c);
+
+    //TEST 4: src vazia
+    char dst7[10] = "ignore";
+    char dst8[10] = "ignore";
+    const char *src3 = "";
+
+    size_t ret_std_d = strlcat(dst7, src3, sizeof(dst7));
+    size_t ret_ft_d = ft_strlcat(dst8, src3, sizeof(dst8));
+
+    ASSERT_STR_EQ(dst7, dst8, "strlcat should keep dst unchanged if src empty");
+    TEST("strlcat should return strlen(dst) if src is empty", ret_std_d == ret_ft_d);
+
+    //TEST 5: dst vazia, espaço suficiente
+    char dst1e[20] = "";
+    char dst2e[20] = "";
+    const char *src4 = "CopyMe";
+
+    size_t ret_std_e = strlcat(dst1e, src4, sizeof(dst1e));
+    size_t ret_ft_e = ft_strlcat(dst2e, src4, sizeof(dst2e));
+
+    ASSERT_STR_EQ(dst1e, dst2e, "strlcat should copy all if dst is empty and buffer is big");
+    TEST("strlcat return value should be strlen(src)", ret_std_e == ret_ft_e);
 
     return (fail);
 }
