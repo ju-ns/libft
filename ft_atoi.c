8ef577d17d0be6f49f2d66fad387e5d9f3cacf1b
Julia Anisio <julia.an.nogueira@gmail.com>
2025-08-03 20:19:56 -0300

    New feature: refactoring ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -13,14 +13,24 @@
 int ft_atoi(const char *nptr)
 {
     int i;
+    int sign;
     int result;
 
     i = 0;
     result = 0;
-    while(nptr[i])
+    sign = 1;
+    while(nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
+        i++;
+    if(nptr[i] == '-' || nptr[i] == '+')
+    {
+        if(nptr[i] == '-')
+            sign = -1;
+        i++;
+    }
+    while(nptr[i] >= '0' && nptr[i] <= '9')
     {
         result = result * 10 + (nptr[i] - '0');
         i++;
-    }
-    return (result);
+    }   
+    return (result * sign);
 }
--- test/test_ft_atoi.c
+++ test/test_ft_atoi.c
@@ -25,13 +25,42 @@
 int main ()
 {
     int fail = 0;
-    //TEST 1: mesmo comportamento do atoi 
+    //TEST 1: numero positivo simples 
     const char *s1 = "1234";
     int sdt_st = atoi(s1);
     int sdt_ft = ft_atoi(s1);
 
-    TEST("Should return same result of sdt_st", sdt_st == sdt_ft);
+    TEST("basic positive number", sdt_st == sdt_ft);
 
+    //TEST 2: com espaços antes 
+    const char *str2 = "   \t\n  123";
+    int std2 = atoi(str2);
+    int ft2  = ft_atoi(str2);
+    TEST("spaces before number", std2 == ft2);
+
+    //TEST 3: numero negativo
+    const char *str3 = "   -456";
+    int std3 = atoi(str3);
+    int ft3  = ft_atoi(str3);
+    TEST("negative number with leading spaces", std3 == ft3);
+
+    //TEST 4: numero com sinal positivo explícito
+    const char *str4 = "+789";
+    int std4 = atoi(str4);
+    int ft4  = ft_atoi(str4);
+    TEST("explicit positive sign", std4 == ft4);
+
+    //TEST 5: string com letras depois 
+    const char *str5 = "42abc";
+    int std5 = atoi(str5);
+    int ft5  = ft_atoi(str5);
+    TEST("number with trailing characters", std5 == ft5);
+
+    //TEST 6: string que começa com letra 
+    const char *str6 = "abc123";
+    int std6 = atoi(str6);
+    int ft6  = ft_atoi(str6);
+    TEST("string with no leading number", std6 == ft6);
     return (fail);
 
 }
